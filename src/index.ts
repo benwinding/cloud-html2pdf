962ae0f4b1fdad79d79169229b861f56c9cf25f5
Ben Winding <ben.winding@gmail.com>
2019-07-16 17:49:19 +0930

    Timeout increased

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,4 @@ const server = app.listen(process.env.PORT || 8080, () => {
   const port = server.address().port;
   console.log(`App listening on port ${port}`);
 });
+server.setTimeout(5 * 60 * 1000);
