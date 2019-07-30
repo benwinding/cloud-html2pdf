3c2b0c1cc5bf60db45e40d5a176e6c457ae909e3
Ben Winding <ben.winding@gmail.com>
2019-07-30 16:22:40 +0930

    Working locally

--- src/pdf-conversion.ts
+++ src/pdf-conversion.ts
@@ -2,30 +2,28 @@ import {
   OptionRequestsAreOk,
   PostRequestsOnly,
   HasBodyProp
-} from './middleware';
-import { Request, Response } from 'express';
-import { writeFileSync, unlinkSync } from 'fs';
-import { tmpdir } from 'os';
-import { join } from 'path';
-import uuidv1 = require('uuid/v1');
-const util = require('util');
-import * as fs from 'fs';
-const writeFilePromise = util.promisify(fs.writeFile);
+} from "./middleware";
+import { Request, Response } from "express";
+import { tmpdir } from "os";
+import { join } from "path";
+import uuidv1 = require("uuid/v1");
+const util = require("util");
+import * as fs from "fs";
 const rmFilePromise = util.promisify(fs.unlink);
 
 export const Html2Pdf = [
   OptionRequestsAreOk,
   PostRequestsOnly,
-  HasBodyProp('html'),
-  HasBodyProp('filename'),
+  HasBodyProp("html"),
+  HasBodyProp("filename"),
   HandleHtml2Pdf
 ];
 
 function tryRemoveFile(filePath) {
-  console.log('pdf-generation: trying to remove file: ', filePath);
+  console.log("pdf-generation: trying to remove file: ", filePath);
   rmFilePromise(filePath).catch(e => {
     // console.error('error removing file: ', e)
-  })
+  });
 }
 
 async function HandleHtml2Pdf(req: Request, res: Response) {
@@ -33,37 +31,32 @@ async function HandleHtml2Pdf(req: Request, res: Response) {
   const { html, filename, imageResolution } = req.body;
 
   try {
-    const Nightmare = require('nightmare');
-    const nightmare = Nightmare({ show: false });
-    console.log('pdf-generation: Begining pdf conversion');
-    const tempHtmlPath = join(tmpdir(), uuidv1() + '.html');
-    await writeFilePromise(tempHtmlPath, html);
-    console.log('pdf-generation: saving tempHtmlPath file: ', tempHtmlPath);
-
-    const tempPdfPath = join(tmpdir(), uuidv1() + '.pdf');
-    await nightmare.goto('file://' + tempHtmlPath).pdf(tempPdfPath);
-    const tempPdfCompressedPath = join(tmpdir(), uuidv1() + '.pdf');
+    console.log("pdf-generation: Begining pdf conversion");
+    const tempPdfPath = join(tmpdir(), uuidv1() + ".pdf");
+    await createPdf(html, tempPdfPath);
+    const tempPdfCompressedPath = join(tmpdir(), uuidv1() + ".pdf");
+    console.log(
+      "pdf-generation: saving tempPdfPath  file: ",
+      tempPdfCompressedPath
+    );
     await compressPdfFile(tempPdfPath, tempPdfCompressedPath, imageResolution);
-    console.log('pdf-generation: saving tempPdfPath  file: ', tempPdfPath);
-    res.download(tempPdfCompressedPath, filename, async (err) => {
+    res.download(tempPdfCompressedPath, filename, async err => {
       if (err) {
-        console.error('pdf-generation: error sending file:', err);
+        console.error("pdf-generation: error sending file:", err);
         res.status(500);
-        res.send('pdf-generation: res.download() error sending file');
+        res.send("pdf-generation: res.download() error sending file");
       }
-      tryRemoveFile(tempHtmlPath);
       tryRemoveFile(tempPdfPath);
       tryRemoveFile(tempPdfCompressedPath);
     });
     // Ensure files are definitely removed after 4 minutes
     const timeOut = 4 * 60 * 1000;
     setTimeout(() => {
-      tryRemoveFile(tempHtmlPath);
       tryRemoveFile(tempPdfPath);
       tryRemoveFile(tempPdfCompressedPath);
     }, timeOut);
   } catch (e) {
-    console.error('pdf-generation: An Error occurred when processing HTML', {
+    console.error("pdf-generation: An Error occurred when processing HTML", {
       e
     });
     res.status(500);
@@ -77,7 +70,23 @@ async function compressPdfFile(
   inputResolution: string
 ): Promise<void> {
   const imageResolution = +inputResolution || 150;
-  const exec = util.promisify(require('child_process').exec);
+  const exec = util.promisify(require("child_process").exec);
   const command = `gs -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -dColorImageResolution=${imageResolution} -q -o ${outputPdfFile} ${inputPdfPath}`;
   await exec(command);
+}
+
+async function createPdf(html: string, outputPdfPath: string) {
+  try {
+    const puppeteer = require("puppeteer");
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: "networkidle2" });
+    await page.pdf({ path: outputPdfPath, format: "A4" });
+    await browser.close();
+  } catch (error) {
+    throw new Error(error);
+  }
 }
